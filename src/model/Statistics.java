bd433f7dbb546bcdc0bbde1431d89c69a1f3b0fb
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-04-11 19:26:51 +0200

    Create Statistics.java

--- src/model/Statistics.java
+++ src/model/Statistics.java
@@ -1,8 +1,15 @@
 package model;
 
+import saveload.SaveData;
+
 public class Statistics {
 
-    public static double getBalanceCurrency(Currency currency){
-
+    public static double getBalanceCurrency(Currency currency) {
+        SaveData sd = SaveData.getInstance();
+        double amount = 0;
+        for (Account account : sd.getAccounts()){
+        if (currency.equals(account.getCurrency())) amount += account.getAmount();
+        }
+        return amount;
     }
 }
