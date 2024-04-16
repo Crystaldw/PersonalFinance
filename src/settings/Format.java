33676b075e49d98d2a37d50e828fab76fb57628a
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-04-16 17:23:08 +0200

    Create HandlerCode.java

--- src/settings/Format.java
+++ src/settings/Format.java
@@ -7,7 +7,7 @@ import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Format {
+final public class Format {
 
     public static String amount(double amount) {
         return String.format(Settings.FORMAT_AMOUNT, amount);
@@ -17,44 +17,44 @@ public class Format {
         return amount(amount) + " " + currency.getCode();
     }
 
-    public static String rate(double rate){
+    public static String rate(double rate) {
         return String.format(Settings.FORMAT_RATE, rate);
     }
 
-    public static String rate(double rate, Currency currency){
+    public static String rate(double rate, Currency currency) {
         return rate(rate) + " " + currency.getCode();
     }
 
-    public static String date(Date date){
+    public static String date(Date date) {
         return dateFormat(date, Settings.FORMAT_DATE);
     }
 
-    public static String dateMonth(Date date){
+    public static String dateMonth(Date date) {
         return dateFormat(date, Settings.FORMAT_DATE_MONTH);
     }
 
-    public static String dateYear(Date date){
+    public static String dateYear(Date date) {
         return dateFormat(date, Settings.FORMAT_DATE_YEAR);
     }
 
-    private static String dateFormat(Date date, String format){
+    private static String dateFormat(Date date, String format) {
         SimpleDateFormat sdf = new SimpleDateFormat(format, new MainDateFormatSymbols());
         return sdf.format(date);
     }
 
-    public static double fromAmountToNumber(String amount)throws NumberFormatException{
+    public static double fromAmountToNumber(String amount) throws NumberFormatException {
         amount = amount.replaceAll(",", ".");
         return Double.parseDouble(amount);
     }
 
-    public static String yesNo(boolean yes){
+    public static String yesNo(boolean yes) {
         if (yes) return Text.get("YES");
         return Text.get("NO");
     }
 
-    public static String getTitleFilter(Filter filter){
+    public static String getTitleFilter(Filter filter) {
         Date time = filter.getTo();
-        switch (filter.getStep()){
+        switch (filter.getStep()) {
             case Filter.STEP_DAY:
                 return date(time);
             case Filter.STEP_MONTH:
@@ -65,7 +65,7 @@ public class Format {
         return null;
     }
 
-    private static class MainDateFormatSymbols extends DateFormatSymbols{
+    private static class MainDateFormatSymbols extends DateFormatSymbols {
         @Override
         public String[] getMonths() {
             return Text.getMonths();
